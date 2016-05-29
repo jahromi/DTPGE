a6264d7f6c1226f4579797e3e65238abdd74849a
Kian Jahromi <knrazeghi@gmail.com>
2016-05-29 16:32:11 -0600

    Update emprical_real_data.m

--- Threshold-Detection/emprical_real_data.m
+++ Threshold-Detection/emprical_real_data.m
@@ -45,7 +45,7 @@ for II=1:K
     end
     DA=ndata(:,:,II);
     p=plot(T,mean(DA));
-    set(p,'LineWidth',2,'color','black','line','--');
+    set(p,'LineWidth',2,'color','black','linestyle','--');
     plot([Result(II,1)+1,Result(II,1)+1],[min(DA(:)),max(DA(:))],'--','LineWidth',2);
     title(sprintf('original gene %d',II));
     %%
@@ -58,7 +58,7 @@ for II=1:K
     end
     DA=D(:,:,II);
     p=plot(T,mean(DA));
-    set(p,'LineWidth',2,'color','black','line','--');
+    set(p,'LineWidth',2,'color','black','linestyle','--');
     plot([Result(II,1)+1,Result(II,1)+1],[min(DA(:)),max(DA(:))],'--','LineWidth',2);
     p=plot([Result(II,1)+1 J-1],[Result(II,4) Result(II,4)]);
     p=plot([Result(II,1)+1 J-1],[Result(II,3) Result(II,3)]);
@@ -67,7 +67,7 @@ for II=1:K
     subplot(2,2,3)
     hold on;
     p=plot(T,mean(DA));
-    set(p,'LineWidth',2,'color','black','line','--');
+    set(p,'LineWidth',2,'color','black','linestyle','--');
     plot([Result(II,1)+1,Result(II,1)+1],[min(DA(:)),max(DA(:))],'--','LineWidth',2);
     p=plot([Result(II,1)+1 J-1],[Result(II,4) Result(II,4)]);
     p=plot([Result(II,1)+1 J-1],[Result(II,3) Result(II,3)]);
@@ -82,7 +82,7 @@ for II=1:K
     end
     DA=SE_D(:,:,II);
     p=plot(T,mean(DA));
-    set(p,'LineWidth',2,'color','black','line','--');
+    set(p,'LineWidth',2,'color','black','linestyle','--');
     plot([Result(II,1)+1,Result(II,1)+1],[min(DA(:)),max(DA(:))],'--','LineWidth',2);
     title(sprintf('Second derevative of gene number %d',II));    
 end
@@ -91,4 +91,58 @@ cnames = {'Gene Number','Change Point','Test Value','Upper Limit','Lower Limit'}
 DA=[(1:II)' Result];
 t = uitable('Data',DA,'ColumnName',cnames,'Position',[0 300 600 600]);
 
-
+function F_T=detector(data,l)
+I=size(data,1); J=size(data,2);
+X_bar=mean(data);
+X_bar=repmat(X_bar,I,1); Z=data-X_bar; S=(Z'*Z)/(I);
+control=0;
+control1=0;
+t=J-5;
+F0=sum((data<=0))/I;
+while control==0
+    JJ=min(t+l,J);
+    %JJ=J;
+    F_0=sum(F0(t:JJ));
+    Exp=(JJ-t+1)*0.5;
+    FF=0;
+    for i=t:JJ
+        for j=i:JJ
+            if i~=j
+                SIGMA=[S(i,i) S(i,j); S(j,i) S(j,j)];
+                mu=[X_bar(1,i); X_bar(1,j)];
+                FF=mvncdf([0;0],mu,SIGMA)+FF;
+            end
+        end
+    end
+    STD=sqrt((0.25*(JJ-t+1)*(t-JJ+1)+2*FF)/I);
+    Z=(F_0-Exp)/STD;
+    if abs(Z)>norminv(1-0.2/2) && control1==0
+        t_warning=t;
+        control1=1;
+    end
+    if abs(Z)>norminv(1-0.05/2)
+        control=1;
+    elseif t==1
+        control=1;
+    else
+        t=t-1;
+    end
+end
+control2=0;
+Z=(F0-0.5)/sqrt(0.25/I);
+if  t~=t_warning
+    for i=t_warning:-1:t+1
+        if control2==0
+            if abs(Z(i))>1.96
+                F_T=i;
+                control2=1;
+            end
+        end
+        if control2==0 && i==t+1
+            F_T=t;
+        end
+    end
+else
+    F_T=t;
+end
+end
