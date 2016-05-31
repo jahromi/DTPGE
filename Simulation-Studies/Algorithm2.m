fc237388aeffc9e243e8cf9d3abb68120bddef0f
Kian Jahromi <knrazeghi@gmail.com>
2016-05-30 18:10:46 -0600

    Update Algorithm2.m

--- Simulation-Studies/Algorithm2.m
+++ Simulation-Studies/Algorithm2.m
@@ -1,8 +1,14 @@
 clearvars; clc;
 global I J K T X;
-I=60; J=25; K=1; R=zeros(J,J); MU=zeros(1,J); D=zeros(1,J); C=19;
+% I is the number of conditions 
+% J number of time points
+% C is the cut point
+I=60; J=25;  C=19;
+%--------------------------------------------------%
+K=1; R=zeros(J,J); MU=zeros(1,J); D=zeros(1,J);
 T=linspace(0,1,J); ro=0.5; alpha=2; ro=ro^(1/J); sigma2=.2;
-t=T;
+t=T;%t is the time vector
+%f is the mean function which is defied as a handle function
 f=@(x)sin(2*pi*x)-4*(x-3/4).^2+1;
 fun=f(t);
 fun(C+1:end)=0;
